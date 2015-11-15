455f58866866f227672f48525ae51e09403be8db
liscju <piotr.listkiewicz@gmail.com>
2015-11-15 12:24:52 +0100

    Add id parameter to star constructors in test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from calculations import Star, G, calculate_forces, calculate_force
 
 class TestStar(TestCase):
     def test_calculate_force_case1(self):
-        s1 = Star(10, (0, 0, 0))
-        s2 = Star(10, (0, 0, 1))
+        s1 = Star(1, 10, (0, 0, 0))
+        s2 = Star(2, 10, (0, 0, 1))
 
         force_result = s1.calculate_force(s2)
 
@@ -16,8 +16,8 @@ class TestStar(TestCase):
         self.assertEqual(force_result, expected_result)
 
     def test_calculate_force_case2(self):
-        s1 = Star(1, (0, 0, 0))
-        s2 = Star(1, (0, 1, 0))
+        s1 = Star(1, 1, (0, 0, 0))
+        s2 = Star(2, 1, (0, 1, 0))
 
         force_result = s1.calculate_force(s2)
 
@@ -25,8 +25,8 @@ class TestStar(TestCase):
         self.assertEqual(force_result, expected_result)
 
     def test_calculate_force_case3(self):
-        s1 = Star(10, (0, 0, 0))
-        s2 = Star(5, (0, 1, 0))
+        s1 = Star(1, 10, (0, 0, 0))
+        s2 = Star(2, 5, (0, 1, 0))
 
         force_result = s1.calculate_force(s2)
 
@@ -36,9 +36,9 @@ class TestStar(TestCase):
 
 class TestCalculationMethods(TestCase):
     def test_calculate_forces_case1(self):
-        s1 = Star(10, (0, 0, 0))
-        s2 = Star(10, (0, 0, 1))
-        s3 = Star(1, (0, 1, 0))
+        s1 = Star(1, 10, (0, 0, 0))
+        s2 = Star(2, 10, (0, 0, 1))
+        s3 = Star(3, 1, (0, 1, 0))
 
         actual_forces = calculate_forces([s1, s2, s3])
 
@@ -46,9 +46,9 @@ class TestCalculationMethods(TestCase):
         self.assertEqual(actual_forces[0], expected_first_result)
 
     def test_calculate_force_case1(self):
-        s1 = Star(10, (0, 0, 0))
-        s2 = Star(10, (0, 1, 0))
-        s3 = Star(5, (0, 1, 0))
+        s1 = Star(1, 10, (0, 0, 0))
+        s2 = Star(2, 10, (0, 1, 0))
+        s3 = Star(3, 5, (0, 1, 0))
 
         actual_forces = calculate_force(s1, [s2, s3])
 
